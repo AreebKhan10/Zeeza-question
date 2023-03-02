136de28cf1c48cc971317ed3bf80a81a047c197b
Dk <119879571+DanielRoy139@users.noreply.github.com>
2023-03-02 20:59:50 +0500

    Ui Changes

--- src/pages/SingleQuestion.js
+++ src/pages/SingleQuestion.js
@@ -21,7 +21,7 @@ export default function SingleQuestion({
   stuDetails,
 }) {
   const [status, setStatus] = useState(false);
-console.log(Data, "<----Data")
+  console.log(Data, "<----Data")
   const Option = singleQuestion?.question[QuesID]?.options?.map(
     (option) => option?.value
   );
@@ -573,197 +573,200 @@ console.log(Data, "<----Data")
   return (
     <div className="studentContent-right">
       <div className="studentContentTop">
-      <div className="menuIcon">
-                <a href="#" className="menuInner" onClick={menuFunction}>
-                  <img src={menu} />
-                </a>
-              </div>
+        <div className="menuIcon">
+          <a href="#" className="menuInner" onClick={menuFunction}>
+            <img src={menu} />
+          </a>
+        </div>
         <p className="text-5xl">{singleQuestion?.title}</p>
         <p className="text-blue-800">
           {QuesID + 1} of {Data[ID].questions.length}
         </p>
       </div>
       <div className="studentContentBottom">
-      <p className="text-xl text-[#002B48]">{value[QuesID]?.replace("[name]", `${stuDetails.FirstName}'s`)}</p>
-      <p className="text-blue-800">
-        {Data[ID].questions[QuesID].description}
-      </p>
+        <p className="text-xl text-[#002B48]">{value[QuesID]?.replace("[name]", `${stuDetails.FirstName}'s`)}</p>
+        <p className="text-blue-800">
+          {Data[ID].questions[QuesID].description}
+        </p>
 
-      {singleQuestion.select === "Multi" ? (
-        <>
-          {singleQuestion.select === "Multi" ? (
-            <>
-              {singleQuestion?.question[QuesID]?.options?.map(
-                (option, index) => {
-                  if (!("isHidden" in option) || option.isHidden === false) {
-                    return (
-                      <div className="selectWrapper">
-                      <li className="flex flex-row items-center">
-                        {option.check === true ? (
-                          <img
-                            src={check}
-                            className={"checkImg"}
-                            onClick={() => handleCheck(index)}
-                          />
-                        ) : (
-                          <img
-                            src={uncheck}
-                            className={"checkImg"}
-                            onClick={() => handleCheck(index)}
-                          />
-                        )}
-
-                        {option.value}
-                      </li>
-                      </div>
-                    );
-                  }
-                }
-              )}
-            </>
-          ) : (
-            <>
+        {singleQuestion.select === "Multi" ? (
+          <>
+            {singleQuestion.select === "Multi" ? (
               <>
-              {singleQuestion?.question[QuesID]?.options?.map(
-                (option, index) => {
-                  if (!("isHidden" in option) || option.isHidden === false) {
-                    return (
-                      <div className="selectWrapper">
-                      <li className="flex flex-row items-center">
-                        {option.check === true ? (
-                          <img
-                            src={check}
-                            className={"checkImg"}
-                            onClick={() => handleCheck(index)}
-                          />
-                        ) : (
-                          <img
-                            src={uncheck}
-                            className={"checkImg"}
-                            onClick={() => handleCheck(index)}
-                          />
-                        )}
+                {singleQuestion?.question[QuesID]?.options?.map(
+                  (option, index) => {
+                    if (!("isHidden" in option) || option.isHidden === false) {
+                      return (
+                        <div className="selectWrapper">
+                          <li className="flex flex-row items-center">
+                            {option.check === true ? (
+                              <img
+                                src={check}
+                                className={"checkImg"}
+                                onClick={() => handleCheck(index)}
+                              />
+                            ) : (
+                              <img
+                                src={uncheck}
+                                className={"checkImg"}
+                                onClick={() => handleCheck(index)}
+                              />
+                            )}
 
-                        {option.value}
-                      </li>
-                      </div>
-                    );
+                            {option.value}
+                          </li>
+                        </div>
+                      );
+                    }
                   }
-                }
-              )}
-            </>
+                )}
+              </>
+            ) : (
+              <>
+                <>
+                  {singleQuestion?.question[QuesID]?.options?.map(
+                    (option, index) => {
+                      if (!("isHidden" in option) || option.isHidden === false) {
+                        return (
+                          <div className="selectWrapper">
+                            <li className="flex flex-row items-center">
+                              {option.check === true ? (
+                                <img
+                                  src={check}
+                                  className={"checkImg"}
+                                  onClick={() => handleCheck(index)}
+                                />
+                              ) : (
+                                <img
+                                  src={uncheck}
+                                  className={"checkImg"}
+                                  onClick={() => handleCheck(index)}
+                                />
+                              )}
 
-              {singleQuestion?.question[QuesID]?.options?.map(
-                (option, index) => (
-                  <li className="block px-4 py-2 hover:bg-gray-100">
-                    <div onClick={() => handleCheck(index)}>
-                      <label class="inline-flex items-center p-2">
-                        <input
-                          type="radio"
-                          class="form-radio"
-                          name="accountType"
-                          value="personal"
-                        />
-                        {/* <span class="ml-2">{option.value}</span> */}
-                        {option.text}
-                      </label>
-                    </div>
-                  </li>
-                )
-              )}
-            </>
-          )}
-        </>
-      ) : singleQuestion.select === "Accordian" ? (
-        <>
-          <AccordianComponent
-            setgoalLevel={setgoalLevel}
-            title={quesTitle}
-            content={quesContents}
-            setMultiLimit={setMultiLimit}
-            singleQuestion={singleQuestion}
-            QuesID={QuesID}
-            MultiLimit={MultiLimit}
-            ID={ID}
-            setData={setData}
-            Data={Data}
-            MultiLimitSub={MultiLimitSub}
-            setMultiLimitSub={setMultiLimitSub}
-          />
-        </>
-      ) : (
-        <div class="selectWrapper">
-          <div class="group relative">
-            <button
-              type="button"
-              class="inline-flex w-full justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm"
-              id="menu-button"
-              aria-expanded="true"
-              aria-haspopup="true"
-            >
-              {/* Select Your answer */}
-              {checkindex >= 0
-                ? Data[ID]?.questions[QuesID]?.options[checkindex]?.value
-                : "Select Option"}
-              <svg
-                class="-mr-1 ml-2 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-            <nav
-              tabindex="0"
-              class="invisible opacity-0 w-full b-2 bg-white border-gray-300 rounded top-full transition-all group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1"
-            >
-              <ul class="py-1">
-                {
-                  <>
-                    {singleQuestion?.question[QuesID]?.options?.map(
-                      (option, index) => {
-                        if (
-                          !("isHidden" in option) ||
-                          option.isHidden === false
-                        ) {
-                          return (
-                            <>
-                           <li className="block px-4 py-2 hover:bg-gray-100">
-                    <div className="singlecheckradio" onClick={() => handleCheck(index)}>
-                       <input
-                          type="radio"
-                          class="form-radio"
-                          name="accountType"
-                          value="personal"
-                          id={`singlecheckradio${index}`}
-                        />
-                      <label class="inline-flex items-center p-2" for={`singlecheckradio${index}`}>
-                        
-                        {/* <span class="ml-2">{option.value}</span> */}
-                        {option.value}
-                      </label>
-                    </div>
-                  </li>
-            </>
-                          );
-                        }
+                              {option.value}
+                            </li>
+                          </div>
+                        );
                       }
-                    )}
-                  </>
-                }
-              </ul>
-            </nav>
+                    }
+                  )}
+                </>
+
+                {singleQuestion?.question[QuesID]?.options?.map(
+                  (option, index) => (
+                    <li className="block px-4 py-2 hover:bg-gray-100">
+                      <div onClick={() => handleCheck(index)}>
+                        <label class="inline-flex items-center p-2">
+                          <input
+                            type="radio"
+                            class="form-radio"
+                            name="accountType"
+                            value="personal"
+                          />
+                          {/* <span class="ml-2">{option.value}</span> */}
+                          {option.text}
+                        </label>
+                      </div>
+                    </li>
+                  )
+                )}
+              </>
+            )}
+          </>
+        ) : singleQuestion.select === "Accordian" ? (
+          <>
+            <AccordianComponent
+              setgoalLevel={setgoalLevel}
+              title={quesTitle}
+              content={quesContents}
+              setMultiLimit={setMultiLimit}
+              singleQuestion={singleQuestion}
+              QuesID={QuesID}
+              MultiLimit={MultiLimit}
+              ID={ID}
+              setData={setData}
+              Data={Data}
+              MultiLimitSub={MultiLimitSub}
+              setMultiLimitSub={setMultiLimitSub}
+            />
+          </>
+        ) : (
+          <div class="selectWrapper">
+            <div class="group relative">
+              <button
+                type="button"
+                class="inline-flex w-full justify-between border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm"
+                id="menu-button"
+                aria-expanded="true"
+                aria-haspopup="true"
+                onClick={showDropdown}
+              >
+                {/* Select Your answer */}
+                {checkindex >= 0
+                  ? Data[ID]?.questions[QuesID]?.options[checkindex]?.value
+                  : "Select Option"}
+                <svg
+                  class="-mr-1 ml-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  id="toggleIcon"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+              <nav
+                tabindex="0"
+                class="invisible opacity-0 w-full b-2 bg-white border-gray-300 top-full transition-all group-focus-within:opacity-100 group-focus-within:border-t-0"
+                id="navigationDropdown"
+              >
+                <ul id="nav-list">
+                  {
+                    <>
+                      {singleQuestion?.question[QuesID]?.options?.map(
+                        (option, index) => {
+                          if (
+                            !("isHidden" in option) ||
+                            option.isHidden === false
+                          ) {
+                            return (
+                              <>
+                                <li className="block px-4 py-2 ">
+                                  <div className="singlecheckradio" onClick={() => handleCheck(index)}>
+                                    <input
+                                      type="radio"
+                                      class="form-radio"
+                                      name="accountType"
+                                      value="personal"
+                                      id={`singlecheckradio${index}`}
+                                    />
+                                    <label class="inline-flex items-center p-2" for={`singlecheckradio${index}`}>
+
+                                      {/* <span class="ml-2">{option.value}</span> */}
+                                      {option.value}
+                                    </label>
+                                  </div>
+                                </li>
+                              </>
+                            );
+                          }
+                        }
+                      )}
+                    </>
+                  }
+                </ul>
+              </nav>
+            </div>
           </div>
-        </div>
-        
-      )}
-    </div>
+
+        )}
+      </div>
     </div>
   );
 }
@@ -772,10 +775,20 @@ function menuFunction() {
   var x = document.getElementById("menuItems");
   var y = document.getElementById("MenuQuestion");
   var z = document.getElementById("studentContentLeft");
-    x.style.display = "none";
-    y.style.display = "none";
-    z.classList.add("openMenu");
+  x.style.display = "none";
+  y.style.display = "none";
+  z.classList.add("openMenu");
 }
+
+function showDropdown() {
+  var nav = document.getElementById("navigationDropdown");
+  var toggleIcon = document.getElementById("toggleIcon");
+  var menuBtn = document.getElementById("menu-button");
+  nav.classList.toggle("openDropdown");
+  toggleIcon.classList.toggle("rotateIcon");
+  menuBtn.classList.toggle("borderNone"); 
+}
+
 
 // function menuFunction() {
 //   var openLeftMenu = document.getElementById("studentContentLeft");
